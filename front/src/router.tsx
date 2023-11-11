f14f85c749b1aef7ea87ced71d8a3d211018d004
Rahul Sengottuvelu <rgs@ramp.com>
2023-11-11 17:53:03 -0500

    .

--- front/src/router.tsx
+++ front/src/router.tsx
@@ -3,17 +3,18 @@ import { useState } from "react";
 import { Stage, Fact } from "./Game";
 import Launcher from "./launcher";
 
-import mockData from "../../backend/data.json";
+import presidentsMockData from "../../backend/presidents.json";
 
 enum Page {
   Launcher = 0,
-  Stage = 1,
+  Presidents = 1,
+  Bridges = 2,
 }
 
 const Router = () => {
   const [page, setPage] = useState(Page.Launcher);
 
-  if (page === Page.Stage) {
+  if (page === Page.Presidents) {
     return (
       <div>
         <Stage
@@ -23,12 +24,33 @@ const Router = () => {
             width: 80,
           }}
           onGoBack={() => setPage(Page.Launcher)}
-          facts={mockData.content.slice(0, 5) as Fact[]}
+          facts={presidentsMockData.content.slice(0, 5) as Fact[]}
+        />
+      </div>
+    );
+  }
+  if (page === Page.Bridges) {
+    return (
+      <div>
+        <Stage
+          world={{
+            name: "fallarbor",
+            height: 80,
+            width: 80,
+          }}
+          onGoBack={() => setPage(Page.Launcher)}
+          facts={presidentsMockData.content.slice(0, 5) as Fact[]}
         />
       </div>
     );
   } else {
-    return <Launcher onSubmit={() => setPage(Page.Stage)} />;
+    return (
+      <Launcher
+        onSubmit={(e) =>
+          setPage(e === "presidents" ? Page.Presidents : Page.Bridges)
+        }
+      />
+    );
   }
 };
 
