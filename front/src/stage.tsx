38bb9c4d38c960d1b3eb7dd99d0224a066a727ad
Ming Ying <ming.ying.nyc@gmail.com>
2023-11-11 12:51:49 -0500

    create dummy image

--- front/src/stage.tsx
+++ front/src/stage.tsx
@@ -1,6 +1,8 @@
 import { Stage as PixiStage, Container, Sprite } from "@pixi/react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import * as PIXI from "pixi.js";
+
+import Pokemon from "./assets/pokemon.png";
 
 enum ArrowKey {
   Left = "ArrowLeft",
@@ -41,16 +43,28 @@ const Stage = () => {
   }, []);
 
   return (
-    <PixiStage width={300} height={300} options={{ backgroundAlpha: 0 }}>
-      <Container x={150} y={150}>
-        <Sprite
-          image="https://pixijs.io/pixi-react/img/bunny.png"
-          x={spritePosition.x}
-          y={spritePosition.y}
-          anchor={new PIXI.Point(0.5, 0.5)}
-        />
-      </Container>
-    </PixiStage>
+    <div className="relative overscroll-none flex justify-center items-center h-screen">
+      <img
+        className="absolute w-[1000px] h-[500px]"
+        src={Pokemon}
+        alt="pokemon"
+      />
+      <PixiStage
+        width={1000}
+        height={500}
+        options={{ backgroundAlpha: 0 }}
+        className="absolute"
+      >
+        <Container x={150} y={150}>
+          <Sprite
+            image="https://pixijs.io/pixi-react/img/bunny.png"
+            x={spritePosition.x}
+            y={spritePosition.y}
+            anchor={new PIXI.Point(0.5, 0.5)}
+          />
+        </Container>
+      </PixiStage>
+    </div>
   );
 };
 
