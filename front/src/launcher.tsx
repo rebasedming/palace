7992627344506a45403a285d2d0287350a3339ca
Olivia Li <oliviali.ouyang@gmail.com>
2023-11-11 17:11:44 -0500

    frontend changes

--- front/src/launcher.tsx
+++ front/src/launcher.tsx
@@ -18,11 +18,12 @@ interface LaunchScreenProps {
 
 interface PromptBoxProps {
   isOpen: boolean;
+  isLoading: boolean;
   onClose: () => void;
   onSubmit: (prompt: string) => void;
 }
 
-const PromptBox = ({ isOpen, onClose, onSubmit }: PromptBoxProps) => {
+const PromptBox = ({ isOpen, onClose, onSubmit, isLoading }: PromptBoxProps) => {
   const [input, setInput] = useState("");
 
   return (
@@ -75,9 +76,19 @@ const PromptBox = ({ isOpen, onClose, onSubmit }: PromptBoxProps) => {
                       input === "" && "bg-opacity-40"
                     )}
                     onClick={() => onSubmit(input)}
-                    disabled={input === ""}
+                    disabled={input === "" || isLoading}
                   >
-                    Create Memory Palace
+                    {isLoading ? (
+                      <>
+                        <svg aria-hidden="true" className="w-4 h-4 mr-3 text-gray-200 animate-spin dark:text-white fill-blue-500" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                          <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                        </svg>
+                        Loading...
+                      </>
+                    ) : (
+                      "Create Memory Palace"
+                    )}
                   </button>
                   <button className="text-xs text-gray-400" onClick={onClose}>
                     Go Back
@@ -88,7 +99,7 @@ const PromptBox = ({ isOpen, onClose, onSubmit }: PromptBoxProps) => {
           </div>
         </div>
       </Dialog>
-    </Transition>
+    </Transition >
   );
 };
 
@@ -126,6 +137,7 @@ const LaunchScreen = ({ onKeyPress }: LaunchScreenProps) => {
 
 const Launcher = ({ onSubmit }: LauncherProps) => {
   const [showPromptBox, setShowPromptBox] = useState(false);
+  const [loading, setLoading] = useState(false)
 
   const handleKeyPress = (event: KeyboardEvent) => {
     if (event.key === ENTER_KEY) setShowPromptBox(true);
@@ -135,8 +147,35 @@ const Launcher = ({ onSubmit }: LauncherProps) => {
     setShowPromptBox(false);
   };
 
-  const onSubmitPrompt = (text: string) => {
-    onSubmit();
+  const onSubmitPrompt = async (text: string) => {
+    const url = 'https://palace-backend.ngrok.app/input';
+
+    // Create the request options
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ text }) // Convert the text object to a JSON string
+    };
+
+    try {
+      // setLoading(true)
+      // const response = await fetch(url, requestOptions);
+
+      // if (!response.ok) {
+      //   throw new Error(`HTTP error! status: ${response.status}`);
+      // }
+
+      // const data = await response.text();
+
+      // console.log(data);
+
+      // setLoading(false)
+      onSubmit();
+    } catch (error) {
+      console.error('Error during fetch:', error.message);
+    }
   };
 
   useEffect(() => {
@@ -154,6 +193,7 @@ const Launcher = ({ onSubmit }: LauncherProps) => {
       </div>
       {showPromptBox && (
         <PromptBox
+          isLoading={loading}
           isOpen={showPromptBox}
           onClose={onClosePromptBox}
           onSubmit={onSubmitPrompt}
