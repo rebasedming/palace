c8d2b20a07cc1b3262c1bc96fa859a31c3f67ad4
Ming Ying <ming.ying.nyc@gmail.com>
2023-11-11 16:38:29 -0500

    Get prize tiles working

--- front/src/router.tsx
+++ front/src/router.tsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import { Stage, Fact } from "./Game";
 import Launcher from "./launcher";
 
+import mockData from "@mock/mnemonics.json";
+
 enum Page {
   Launcher = 0,
   Stage = 1,
@@ -21,13 +23,7 @@ const Router = () => {
             width: 80,
           }}
           onGoBack={() => setPage(Page.Launcher)}
-          facts={[
-            {
-              url: "https://fastly.picsum.photos/id/427/536/354.jpg?hmac=L9xt4dPYu1OBmkl4Md1KF51PpeBgp_mqgmsi4TxU0Mw",
-              mnemonic: "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z",
-              fact: "A is for apple, B is for ball, C is for cat, D is for dog, E is for elephant, F is for fish, G is for goat, H is for horse, I is for igloo, J is for jellyfish, K is for kangaroo, L is for lion, M is for monkey, N is for nest, O is for octopus, P is for pig, Q is for queen, R is for rabbit, S is for snake, T is for tiger, U is for umbrella, V is for violin, W is for whale, X is for x-ray, Y is for yak, Z is for zebra",
-            },
-          ]}
+          facts={mockData.content.slice(0, 5) as Fact[]}
         />
       </div>
     );
